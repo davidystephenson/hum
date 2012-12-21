a9741bc8604caf3c66b869f7bde7efd30757d227
David Stephenson <davidystephenson@gmail.com>
2012-12-20 21:14:33 -0500

    removed tabbing from main, box, and index

--- Box.hx
+++ Box.hx
@@ -3,50 +3,50 @@ import flash.events.MouseEvent;
 import flash.geom.ColorTransform;
 
 class Box extends MovieClip {
-	public var hum:Hum;
-	public var row:Int;
-	public var column:Int;
-	public var selected:Bool;
-	public var size:Float;
+    public var hum:Hum;
+    public var row:Int;
+    public var column:Int;
+    public var selected:Bool;
+    public var size:Float;
 
-	public function new(myHum:Hum, myRow:Int, myColumn:Int) {
-		super();
+    public function new(myHum:Hum, myRow:Int, myColumn:Int) {
+        super();
         hum = myHum;
-		row = myRow;
-		column = myColumn;
+        row = myRow;
+        column = myColumn;
 
-		// Boxes are not selected by default
-		selected = false;
-		
-		// Check the score to see if the Box is selected
-		if(hum.score[row][column]) { graphics.beginFill(hum.red); selected = true; }
-		else { graphics.beginFill(hum.blue); selected = false; }
+        // Boxes are not selected by default
+        selected = false;
+        
+        // Check the score to see if the Box is selected
+        if(hum.score[row][column]) { graphics.beginFill(hum.red); selected = true; }
+        else { graphics.beginFill(hum.blue); selected = false; }
 
-		// Each Box should be an equal portion of the hum
+        // Each Box should be an equal portion of the hum
         /// Determine if there is less room for height or width to fit all rows and columns
-		size = Math.min(hum.depth / hum.rowNum, hum.span / hum.colNum);
+        size = Math.min(hum.depth / hum.rowNum, hum.span / hum.colNum);
         /// Draw the square
-		graphics.drawRect(0, 0, size - 1, size -1);
+        graphics.drawRect(0, 0, size - 1, size -1);
         /// Place the box
-		x = column * size;
-		y = row * size;
+        x = column * size;
+        y = row * size;
 
-		addEventListener(MouseEvent.MOUSE_DOWN, toggle);
-		hum.canvas.addChild(this);
-	} // public function new(grid:MovieClip, columnNumber:Int, rowNumber:Int)
+        addEventListener(MouseEvent.MOUSE_DOWN, toggle);
+        hum.canvas.addChild(this);
+    } // public function new(grid:MovieClip, columnNumber:Int, rowNumber:Int)
 
-	public function toggle(event:MouseEvent) {
-		var my_color:ColorTransform = new ColorTransform();
-		if(selected) {
-			selected = false;
-			hum.score[row][column] = 0;
-			my_color.color = hum.blue;
-		} else {
-			selected = true;
-			hum.score[row][column] = 1;
-			my_color.color = hum.red;
-		}
-		transform.colorTransform = my_color;
-	} // public function toggle(event:MouseEvent)
+    public function toggle(event:MouseEvent) {
+        var my_color:ColorTransform = new ColorTransform();
+        if(selected) {
+            selected = false;
+            hum.score[row][column] = 0;
+            my_color.color = hum.blue;
+        } else {
+            selected = true;
+            hum.score[row][column] = 1;
+            my_color.color = hum.red;
+        }
+        transform.colorTransform = my_color;
+    } // public function toggle(event:MouseEvent)
 } // class Box extends MovieClip
 
--- Main.hx
+++ Main.hx
@@ -3,25 +3,25 @@ import flash.display.MovieClip;
 
 class Main extends MovieClip {
 
-	public var console:ConsoleSender;
-	public var canvas:MovieClip;
-	public var hum:Hum;
+    public var console:ConsoleSender;
+    public var canvas:MovieClip;
+    public var hum:Hum;
 
-	public function new() {
-		super();
-		
+    public function new() {
+        super();
+        
         // Prep debugging
-		console = new ConsoleSender();
+        console = new ConsoleSender();
 
         // Declare the canvas
-		canvas = flash.Lib.current;
+        canvas = flash.Lib.current;
 
         // Create a Hum
-		hum = new Hum(canvas);
+        hum = new Hum(canvas);
 
-	} // public function new()
+    } // public function new()
 
-	static function main() {
-		Lib.current.stage.addChild(new Main());
-	} // static function main()
+    static function main() {
+        Lib.current.stage.addChild(new Main());
+    } // static function main()
 } // class Main
